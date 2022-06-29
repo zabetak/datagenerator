b3eff748a9a355fdac445f99752fed81bb97027e
Stamatis Zampetakis <zabetak@gmail.com>
2022-06-29 12:51:39 +0200

    Generate random timestamps in the year internal [0000-9999] with nano precision
    Makes the default timestamp generation more widespread than having everything in the same day.

--- src/main/java/com/github/zabetak/datagenerator/TimestampColumnGenerator.java
+++ src/main/java/com/github/zabetak/datagenerator/TimestampColumnGenerator.java
@@ -16,14 +16,36 @@
  */
 package com.github.zabetak.datagenerator;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
 
 /**
  * Generates data for columns of type timestamp.
  */
-public final class TimestampColumnGenerator
-    implements ColumnGenerator<LocalDateTime> {
-  public LocalDateTime generate() {
-    return LocalDateTime.now();
+public final class TimestampColumnGenerator implements ColumnGenerator<String> {
+
+  private static final DateTimeFormatter FORMATTER =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.nnnnnnnnn");
+
+  private final Random rand = new Random();
+
+  @Override
+  public String generate() {
+    int randYear = rand.nextInt(9999) + 1;
+    int randMonth = rand.nextInt(12) + 1;
+    LocalDate yearMonth = LocalDate.of(randYear, randMonth, 1);
+    int randdays = rand.nextInt(yearMonth.lengthOfMonth()) + 1;
+    LocalDate fullDate = yearMonth.plus(randdays, ChronoUnit.DAYS);
+    int randHour = rand.nextInt(24);
+    int randMinutes = rand.nextInt(60);
+    int randSec = rand.nextInt(60);
+    int randNano = rand.nextInt(1_000_000_000);
+    LocalTime time = LocalTime.of(randHour, randMinutes, randSec, randNano);
+    return LocalDateTime.of(fullDate, time).format(FORMATTER);
   }
+
 }
